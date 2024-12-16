8b60ced96cc7b561a2857d060ca85f5affd1d787
p@ren <ntimprosper308@gmail.com>
2024-12-16 20:37:32 +0000

    Create new file
    Add Node4.py file

---
+++ Node4.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.data = None
+        self.left = None
+        
+def leveOderTraversal(root):
+    if not root:
+        return
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        print(node.data, end=" ")
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+            
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+leveOderTraversal(root)
